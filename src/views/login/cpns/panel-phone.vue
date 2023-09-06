efce3adaf9b16c0d9a61fb0ad1e65df7005b72cc
zhangyu <1015371461@qq.com>
2023-09-07 00:46:04 +0800

    feat: 手机号登录功能

--- src/views/login/cpns/panel-phone.vue
+++ src/views/login/cpns/panel-phone.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="phone">
-    <h2>phone</h2>
-  </div>
+  <el-form label-width="60px">
+    <el-form-item label="手机号">
+      <el-input />
+    </el-form-item>
+    <el-form-item label="验证码">
+      <div class="flex gap-x-2">
+        <el-input />
+        <el-button type="primary">获取验证码</el-button>
+      </div>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script setup lang="ts"></script>
