16368b82acc238cc5e0338fd9fe6136b1ab90e4f
zhangyu <1015371461@qq.com>
2023-09-07 11:11:26 +0800

    feat: 拦截器token

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,22 +1,20 @@
 // service统一出口
 import ZYRequest from './request/index';
 import { BASE_URL, TIME_OUT } from './config/index';
-// import { LOGIN_TOKEN } from '@/global/constants'
-// import { localCache } from '@/utils/cache'
+import { localCache } from '@/utils/cache';
 
 const zyRequest = new ZYRequest({
   baseURL: BASE_URL,
   timeout: TIME_OUT,
-  // 拦截器
+  // 拦截器:每一个请求都自动携带token
   interceptors: {
-    // 每一个请求都自动携带token
-    // requestSuccessFn: (config) => {
-    //   const token = localCache.getCache(LOGIN_TOKEN);
-    //   if (config.headers && token) {
-    //     config.headers.Authorization = 'Bearer ' + token;
-    //   }
-    //   return config;
-    // },
+    requestSuccessFn: (config) => {
+      const token = localCache.getCache('token');
+      if (config.headers && token) {
+        config.headers.Authorization = 'Bearer ' + token;
+      }
+      return config;
+    },
   },
 });
 
